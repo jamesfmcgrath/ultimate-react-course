d2ed955ae80ae7e55479d4cac4609b051cef68c0
James McGrath <james@mcgrathmedia.com>
2026-02-02 13:04:08 +0000

    Section 18 challenge

--- 10-react-quiz/react-quiz/src-no-context/components/Options.js
+++ 10-react-quiz/react-quiz/src-no-context/components/Options.js
@@ -1,18 +1,15 @@
-import { useQuiz } from '../contexts/QuizContext';
-function Options({ question }) {
-  const { dispatch, answer } = useQuiz();
+function Options({ question, dispatch, answer }) {
   const hasAnswer = answer !== null;
-
   return (
     <div className="options">
       {question.options.map((option, index) => (
         <button
           className={`btn btn-option ${index === answer ? 'answer' : ''} ${
-            hasAnswer ?
-              index === question.correctOption ?
-                'correct'
-              : 'wrong'
-            : ''
+            hasAnswer
+              ? index === question.correctOption
+                ? 'correct'
+                : 'wrong'
+              : ''
           }`}
           key={option}
           disabled={answer !== null}
