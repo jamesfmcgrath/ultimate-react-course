3de559c20ed41c08c6c33db3d53a2fbf948a4b38
James McGrath <james@mcgrathmedia.com>
2026-02-02 13:04:08 +0000

    Section 18 challenge

--- 10-react-quiz/react-quiz/src-no-context/components/Timer.js
+++ 10-react-quiz/react-quiz/src-no-context/components/Timer.js
@@ -1,8 +1,6 @@
 import { useEffect } from 'react';
-import { useQuiz } from '../contexts/QuizContext';
 
-function Timer() {
-  const { dispatch, secondsRemaining } = useQuiz();
+function Timer({ dispatch, secondsRemaining }) {
   const mins = Math.floor(secondsRemaining / 60);
   const seconds = secondsRemaining % 60;
 
@@ -14,7 +12,7 @@ function Timer() {
 
       return () => clearInterval(id);
     },
-    [dispatch],
+    [dispatch]
   );
 
   return (
